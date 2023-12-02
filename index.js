604a2a69e0261735abd9204dba67bd8a0463754f
yakubiq raven <yakubiq@proton.me>
2023-12-02 23:05:06 +0100

    express changes

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const express = require('express');
+
 const {readFile, readFileSync} = require('fs');
 const app = express();
+
 
 app.use(express.static('public'));
 
 app.get('/', (request, response) => {
         readFile('./index.html', 'utf8', (err, html) =>{
         if(err){
-            response.status(500).send("Internal server error 500")
+            ise();
         }
         response.send(html);
     })
 });
 
+app.get('*' , (request, response) => {
+    readFile('./404.html', 'utf8', (err, html) =>{
+        if(err){
+            ise();
+        }
+        response.send(html);
+    }) 
+});
+
 app.listen(process.env.PORT || 80, () => console.log('Site is running | http://localhost:80'));
+
+
+//sslServer.listen(3443, () => console.log("Secure server on port 3443 localhost:3443"));
+
+
+function ise(){
+            response.status(500).send("Internal server error 500")
+}
