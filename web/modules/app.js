727b909dcc3222f1933cf3be2db0f2af377a0b72
raven <yakubiq@proton.me>
2024-04-02 21:38:22 +0200

    removed hover+click

--- web/modules/app.js
+++ web/modules/app.js
@@ -85,11 +85,11 @@ particlesJS('particles-js',
       "detect_on": "canvas",
       "events": {
         "onhover": {
-          "enable": true,
+          "enable": false,
           "mode": "grab"
         },
         "onclick": {
-          "enable": true,
+          "enable": false,
           "mode": "push"
         },
         "resize": true
